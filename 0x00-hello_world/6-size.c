8cbde8043192abaf344116bdb0a018413d1e4069
Rokaya <rokayaamgad4@gmail.com>
2023-03-16 17:04:38 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int main(void)
 	long long int d;
 	float f;
 
-printf("Size of a char: %zu byte(s)\n",sizeof(char));
-printf("Size of an int: %zu byte(s)\n",sizeof(int));
-printf("Size of a long int: %zu byte(s)\n",sizeof(long int);
-printf("size of a long long int: %zu byte(s)\n",sizeof(long long int));
-printf("size of a float: %zu byte(s)\n",sizeof(float));
+printf("Size of a char: %zu byte(s)\n",sizeof(a));
+printf("Size of an int: %zu byte(s)\n",sizeof(b));
+printf("Size of a long int: %zu byte(s)\n",sizeof(c);
+printf("size of a long long int: %zu byte(s)\n",sizeof(d));
+printf("size of a float: %zu byte(s)\n",sizeof(f));
 return (0);
 }

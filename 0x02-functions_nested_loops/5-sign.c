9422ce599b731e313497356fb7076e57692ba008
Rokaya <rokayaamgad4@gmail.com>
2023-03-21 15:55:00 -0700

    5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
  * print_sign - Prints the sign of a number
- * @n: The number ti be checked 
+ * @n:The number to be checked
  * Return: 1 for positive num, -1 for negative num or zero fot anything else
  */
 
